42260926c3aed2aed767fd135d6c6b1a09d5fcdc
Anne-Hélène <ahbricet@gmail.com>
2025-09-24 17:19:49 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,68 @@
+const postsArray = [
+  {
+    name: "Blog One",
+    image: "/images/desk-pc.png",
+    id: 0,
+    date: "July 23, 2022",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+  {
+    name: "Blog two",
+    image: "/images/image-04.png",
+    id: 1,
+    date: "July 23, 2022",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+  {
+    name: "Blog One",
+    image: "/images/desk-pc.png",
+    id: 2,
+    date: "July 23, 2022",
+    title: "Blog One",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+  {
+    name: "Blog three",
+    image: "/images/desk-pc.png",
+    id: 3,
+    date: "July 23, 2022",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+  {
+    name: "Blog faur",
+    image: "/images/desk-pc.png",
+    id: 4,
+    date: "July 23, 2022",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+  {
+    name: "Blog five",
+    image: "/images/desk-pc.png",
+    id: 5,
+    date: "July 23, 2022",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+  {
+    name: "Blog six",
+    image: "/images/desk-pc.png",
+    id: 6,
+    date: "July 23, 2022",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+  {
+    name: "Blog seven",
+    image: "/images/desk-pc.png",
+    id: 7,
+    date: "July 23, 2022",
+    description:
+      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+  },
+];
+export default postsArray;
