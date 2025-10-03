52c24fe6214507b0f00db702c377442ca157059e
Anne-Hélène <ahbricet@gmail.com>
2025-10-03 11:43:05 +0200

    Add btn read more in js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import postsArray from "./data.js";
 
 let realisationsInHtml = [];
-let limit = 0;
 
-function displayArticles() {
-  const articesToShow = limit ? postsArray.slice(0, limit) : postsArray;
+function displayArticles(start, end) {
   return postsArray
+    .slice(start, end)
     .map(function (article) {
-      console.log(postsArray);
       return `
-            <article class="post" id="post-one">
+            <article class="post">
               <img
                 src="${article.image}"
                 class="blog-img"
@@ -27,7 +25,13 @@ function displayArticles() {
     .join(" ");
 }
 
-realisationsInHtml = displayArticles();
-document.getElementById("posts-list").innerHTML = realisationsInHtml;
-const moreBtn = document.getElementsById("more-btn");
-console.log(realisationsInHtml);
+const postList = document.getElementById("posts-list");
+postList.innerHTML = displayArticles(0, 3);
+
+const moreBtn = document.getElementById("more-btn");
+moreBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const remainingArticles = displayArticles(3, postsArray.length);
+  postList.innerHTML += remainingArticles;
+  moreBtn.style.display = "none";
+});
