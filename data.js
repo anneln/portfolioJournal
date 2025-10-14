ce59af0ebbd37b15787b1b113491914790657a81
Anne-Hélène <ahbricet@gmail.com>
2025-10-14 16:58:17 +0200

    Update data.js
    test another import for displaying images on local AND on netlify !

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
+const orderimg = new URL("./images/orderfood.jpg", import.meta.url).href;
+
 const postsArray = [
   {
     name: "Twimba",
-    image: "./images/twimba.jpg",
+    image: "../images/twimba.jpg",
     subtitle: "This is a solo project to clone twitter and Scrimba.",
     id: 0,
     date: "August 05, 2025",
@@ -11,7 +13,7 @@ const postsArray = [
   },
   {
     name: "Mobile Ordering App",
-    image: "/images/orderfood.jpg",
+    image: orderimg,
     subtitle:
       "This is a solo project to create a mobile restaurant ordering application.",
     id: 1,
