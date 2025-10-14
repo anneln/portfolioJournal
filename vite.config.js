81e36f0fe75e1bd51ea053fb2c20e559810d85e1
Anne-Hélène <ahbricet@gmail.com>
2025-10-14 09:09:35 +0200

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,35 @@
-import { defineConfig } from "vite";
+/*import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
+});*/
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        article: resolve(__dirname, "article.html"),
+        aboutMe: resolve(__dirname, "about-me.html"),
+      },
+      output: {
+        entryFileNames: "js/[name].js",
+        chunkFileNames: "js/[name]-[hash].js",
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          const ext = info[info.length - 1];
+          if (/png|jpe?g/i.test(ext)) {
+            return `images/[name]-[hash][extname]`;
+          } else if (ext === "css") {
+            return `css/[name]-[hash][extname]`;
+          }
+          return `[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
 });
